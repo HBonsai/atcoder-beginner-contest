38171e4824de2f6c841c814e42d2fb9683fef289
HBonsai <hbonbyaku@gmail.com>
2018-07-21 21:03:39 +0900

    入力を受け付ける部分を追加

--- src/abc103/a/Main.java
+++ src/abc103/a/Main.java
@@ -5,5 +5,10 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int a1 = sc.nextInt();
+		int a2 = sc.nextInt();
+		int a3 = sc.nextInt();
+
+
 	}
 }

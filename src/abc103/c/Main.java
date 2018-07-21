f78b971b79901c9e0ee1431fed1af2c8853d5adb
HBonsai <hbonbyaku@gmail.com>
2018-07-21 22:41:25 +0900

    不正解。テスト終了時の回答をコミット

--- src/abc103/c/Main.java
+++ src/abc103/c/Main.java
@@ -10,7 +10,20 @@ public class Main {
 		for (int i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
+		// fはどんなに大きくてもm * anを超えないはず。
+		int m_max = 100000;
+		int f[] = new int[m_max + 1];
+		for (int m = 0; m <= m_max; m++) {
+			for (int i = 0; i < n; i++) {
+				f[m] += m%a[i];
+			}
+		}
 
-
+		// fの最大値を求める
+		int max_temp = f[0];
+		for (int i = 0; i < f.length - 1; i++) {
+			max_temp = Math.max(max_temp, f[i+1]);
+		}
+		System.out.println(max_temp);
 	}
 }

60a133034751e1b87edad2fa4f13728ff341b0b9
HBonsai <hbonbyaku@gmail.com>
2018-07-21 21:15:13 +0900

    計画を立てた

--- src/abc103/a/Main.java
+++ src/abc103/a/Main.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a1 = sc.nextInt();
-		int a2 = sc.nextInt();
-		int a3 = sc.nextInt();
-
-
+		// A1-A3を受け付ける
+		int a[] = new int[3];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		// 3通りの絶対値を格納する配列Xを作成する
+		// 3通りの絶対値を合計する
+		// 3通りの中で最も大きいものを合計から引く
+		// 最低コストmin_costを出力する
 	}
 }

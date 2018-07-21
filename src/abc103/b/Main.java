9efe5611d561b11e272aa188652a4f3245e67f76
HBonsai <hbonbyaku@gmail.com>
2018-07-21 21:33:59 +0900

    B問題を作成。計画を立てる

---
+++ src/abc103/b/Main.java
@@ -0,0 +1,12 @@
+package abc103.b;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String t = sc.next();
+		// Sを1回転させる。Tと一致したらYesしてbreak
+	}
+}

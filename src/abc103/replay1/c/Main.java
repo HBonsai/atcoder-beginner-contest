4d1a6edb536562a7bb0163305c87e8563eb36339
HBonsai <hbonbyaku@gmail.com>
2018-07-22 08:10:30 +0900

    C問題がAcceptされたのでコミット

--- src/abc103/replay1/c/Main.java
+++ src/abc103/replay1/c/Main.java
@@ -12,5 +12,11 @@ public class Main {
 		// このとき、m%aiの各項が最大になるようなmは、m0-1
 		// なぜなら m mod ai のすべての項が ai-1になるから
 		// よってf(m)の最大値は f(m0-1) = (a1 - 1) + (a2 - 1) +..+ (aN - 1)
+		int fm_max = 0;
+		for (int i = 0; i < N; i++) {
+			int ai = sc.nextInt();
+			fm_max += ai - 1;
+		}
+		System.out.println(fm_max);
 	}
 }
